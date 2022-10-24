af1e976fc543e47c7ad50ed8773d6dd17bb67fb9
Konosuke Kachi <github@s.k-kachi.net>
2022-10-24 17:15:33 +0900

    Update `PromiseCache`

--- test/promise-cache.test.ts
+++ test/promise-cache.test.ts
@@ -1,6 +1,7 @@
 import {PromiseCache} from '../src/promise-cache';
 
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+const sleep = (ms: number, rejects = false) =>
+  new Promise((resolve, reject) => setTimeout(rejects ? reject : resolve, ms));
 
 describe('PromiseCache ', () => {
   test('resolves immediately (lazy=false)', async () => {
@@ -52,8 +53,9 @@ describe('PromiseCache ', () => {
       },
     });
     expect(cache.get()).rejects.toBe(true);
-    await sleep(1);
+    await sleep(0);
     expect(promiseGenerator).toBeCalledTimes(2);
+    expect(cache.get()).resolves.toBe(true);
   });
 
   test('rejects and then resolves (lazy=true)', async () => {
@@ -72,8 +74,12 @@ describe('PromiseCache ', () => {
       },
     });
     expect(cache.get()).rejects.toBe(true);
-    await sleep(1);
+    await sleep(0);
     expect(promiseGenerator).toBeCalledTimes(1);
+    expect(cache.get()).resolves.toBe(true);
+    await sleep(0);
+    expect(promiseGenerator).toBeCalledTimes(2);
+    expect(cache.get()).resolves.toBe(true);
   });
 
   test('resolves many times (lazy=false)', async () => {
@@ -84,10 +90,12 @@ describe('PromiseCache ', () => {
         lazy: false,
       },
     });
+    await sleep(0);
+    expect(promiseGenerator).toBeCalledTimes(1);
     for (let i = 0; i < 10; i++) {
       expect(cache.get()).resolves.toBe(true);
     }
-    await sleep(1);
+    await sleep(0);
     expect(promiseGenerator).toBeCalledTimes(1);
   });
 
@@ -99,10 +107,66 @@ describe('PromiseCache ', () => {
         lazy: true,
       },
     });
+    await sleep(0);
+    expect(promiseGenerator).toBeCalledTimes(0);
     for (let i = 0; i < 10; i++) {
       expect(cache.get()).resolves.toBe(true);
     }
-    await sleep(1);
+    await sleep(0);
     expect(promiseGenerator).toBeCalledTimes(1);
+  });
+
+  test('maxRetries', async () => {
+    const promiseGenerator = jest.fn(() => Promise.reject(true));
+    const cache = new PromiseCache({
+      promiseGenerator,
+      options: {
+        maxRetries: 2,
+      },
+    });
+    await sleep(0);
+    expect(cache.get()).rejects.toBe(true);
+    expect(promiseGenerator).toBeCalledTimes(3);
+  });
+
+  test('minRetryInterval', async () => {
+    let init = true;
+    const promiseGenerator = jest.fn(() => {
+      if (init) {
+        init = false;
+        return sleep(20, true); // init -(20ms)-> reject
+      }
+      return sleep(20); // retry -(20ms)-> resolve
+    });
+    const cache = new PromiseCache({
+      promiseGenerator,
+      options: {
+        minRetryInterval: 40, // init -(40ms)-> retry
+      },
+    });
+
+    // init -(20ms)-> reject -(20ms)-> retry -(20ms)-> resolve
+
+    // ---   0 ms ---
+    await sleep(0);
+    const p0 = cache.get();
+    expect(promiseGenerator).toBeCalledTimes(1);
+    // ---  10 ms ---
+    await sleep(10);
+    const p1 = cache.get();
+    expect(promiseGenerator).toBeCalledTimes(1);
+    // ---  30 ms ---
+    await sleep(20);
+    const p2 = cache.get();
+    expect(promiseGenerator).toBeCalledTimes(1);
+    // --- 50 ms ---
+    await sleep(20);
+    const p3 = cache.get();
+    expect(promiseGenerator).toBeCalledTimes(2);
+
+    expect(p0).rejects.toBe(undefined);
+    expect(p1).rejects.toBe(undefined);
+    expect(p2).resolves.toBe(undefined);
+    expect(p3).resolves.toBe(undefined);
   });
 });
